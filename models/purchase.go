1434c5e73e8ef3830d0b94a6c45a991b3c0e4989
DayvonAllen <dayvonallen93@outlook.com>
2021-12-22 20:33:31 -0600

    added validation for product price

--- models/purchase.go
+++ models/purchase.go
@@ -6,29 +6,26 @@ import (
 )
 
 type Purchase struct {
-	Id              primitive.ObjectID `bson:"_id" json:"id"`
-	FirstName       string             `bson:"firstName" json:"firstName" validate:"max=60,min=1"`
-	LastName        string             `bson:"lastName" json:"lastName" validate:"max=60,min=1"`
-	Email           string             `bson:"email" json:"email" validate:"max=60,min=1"`
-	StreetAddress   string             `bson:"streetAddress" json:"streetAddress" validate:"max=60,min=1"`
-	OptionalAddress string             `bson:"optionalAddress" json:"optionalAddress" validate:"max=60"`
-	City            string             `bson:"city" json:"city" validate:"max=60,min=1"`
-	State           string             `bson:"state" json:"state" validate:"max=60,min=1"`
-	ZipCode         string             `bson:"zipCode" json:"zipCode" validate:"max=60,min=1"`
-	PurchasedItems  *[]Product         `bson:"purchasedItems" json:"purchasedItems"`
-	FinalPrice      int16              `bson:"finalPrice" json:"finalPrice"`
-	CouponUsed      Coupon             `bson:"couponUsed" json:"couponUsed"`
-	//CreditCardNumber         string             `bson:"creditCardNumber" json:"creditCardNumber" validate:"max=60,min=1"`
-	//CreditCardExpirationDate string             `bson:"creditCardExpirationDate" json:"creditCardExpirationDate" validate:"max=60,min=1"`
-	//CreditCardSecurityCode   string             `bson:"creditCardSecurityCode" json:"creditCardSecurityCode" validate:"max=60,min=1"`
-	PurchaseConfirmationId string    `bson:"purchaseConfirmationId" json:"purchaseConfirmationId"`
-	TrackingId             string    `bson:"trackingId" json:"trackingId"`
-	Tax                    int16     `bson:"tax" json:"tax"`
-	Shipped                bool      `bson:"shipped" json:"shipped"`
-	Delivered              bool      `bson:"delivered" json:"delivered"`
-	InfoEmailOptIn         bool      `bson:"infoEmailOptIn" json:"infoEmailOptIn"`
-	CreatedAt              time.Time `bson:"createdAt" json:"-"`
-	UpdatedAt              time.Time `bson:"updatedAt" json:"-"`
+	Id                     primitive.ObjectID `bson:"_id" json:"id"`
+	FirstName              string             `bson:"firstName" json:"firstName" validate:"max=60,min=1"`
+	LastName               string             `bson:"lastName" json:"lastName" validate:"max=60,min=1"`
+	Email                  string             `bson:"email" json:"email" validate:"max=60,min=1"`
+	StreetAddress          string             `bson:"streetAddress" json:"streetAddress" validate:"max=60,min=1"`
+	OptionalAddress        string             `bson:"optionalAddress" json:"optionalAddress" validate:"max=60"`
+	City                   string             `bson:"city" json:"city" validate:"max=60,min=1"`
+	State                  string             `bson:"state" json:"state" validate:"max=60,min=1"`
+	ZipCode                string             `bson:"zipCode" json:"zipCode" validate:"max=60,min=1"`
+	PurchasedItems         *[]Product         `bson:"purchasedItems" json:"purchasedItems"`
+	FinalPrice             int16              `bson:"finalPrice" json:"finalPrice"`
+	CouponUsed             Coupon             `bson:"couponUsed" json:"couponUsed"`
+	PurchaseConfirmationId string             `bson:"purchaseConfirmationId" json:"purchaseConfirmationId"`
+	TrackingId             string             `bson:"trackingId" json:"trackingId"`
+	Tax                    int16              `bson:"tax" json:"tax"`
+	Shipped                bool               `bson:"shipped" json:"shipped"`
+	Delivered              bool               `bson:"delivered" json:"delivered"`
+	InfoEmailOptIn         bool               `bson:"infoEmailOptIn" json:"infoEmailOptIn"`
+	CreatedAt              time.Time          `bson:"createdAt" json:"-"`
+	UpdatedAt              time.Time          `bson:"updatedAt" json:"-"`
 }
 
 type PurchaseAddressDTO struct {
