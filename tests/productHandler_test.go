dd5ab7c81d2938c2bf13708cad9f9e315e8db79b
DayvonAllen <dayvonallen93@outlook.com>
2021-12-28 01:24:11 -0600

    added unit tests

--- tests/productHandler_test.go
+++ tests/productHandler_test.go
@@ -1,7 +1,9 @@
 package tests
 
 import (
+	"bytes"
 	"context"
+	"fmt"
 	"freq/database"
 	"freq/models"
 	"freq/router"
@@ -13,6 +15,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"strings"
 	"testing"
@@ -146,6 +149,147 @@ func TestProductHandler_FindAll(t *testing.T) {
 			if err2 != nil {
 				return
 			}
+		}
+
+		// Perform the request plain with the app.
+		// The -1 disables request latency.
+		res, err := app.Test(req, -1)
+
+		// verify that no error occured, that is not expected
+		assert.Equalf(t, test.expectedError, err != nil, test.description)
+
+		// As expected errors lead to broken responses, the next
+		// test case needs to be processed
+		if test.expectedError {
+			continue
+		}
+
+		// Verify if the status code is as expected
+		assert.Equalf(t, test.expectedCode, res.StatusCode, test.description)
+
+		// Read the response body
+		body, err := ioutil.ReadAll(res.Body)
+
+		// Reading the response body should work everytime, such that
+		// the err variable should be nil
+		assert.Nilf(t, err, test.description)
+
+		// Verify, that the response body equals the expected body
+		assert.Equalf(t, test.expectedBody, strings.Contains(string(body), test.evaluator), test.description)
+	}
+}
+
+func TestProductHandler_Create(t *testing.T) {
+	p, err := dockertest.NewPool("")
+	if err != nil {
+		log.Fatalf("could not connect to docker: %s", err)
+	}
+
+	pool = p
+
+	opts := dockertest.RunOptions{
+		Repository:   "mongo",
+		Tag:          "latest",
+		ExposedPorts: []string{"27017"},
+		PortBindings: map[docker.Port][]docker.PortBinding{
+			"27017": {
+				{HostIP: "0.0.0.0", HostPort: "27017"},
+			},
+		},
+	}
+
+	resource, err = pool.RunWithOptions(&opts)
+	if err != nil {
+		_ = pool.Purge(resource)
+		log.Fatalf("could not start resource: %s", err)
+	}
+
+	os.Setenv("DB_URL", "mongodb://localhost:27017")
+
+	defer func(pool *dockertest.Pool, r *dockertest.Resource) {
+		err := pool.Purge(r)
+		if err != nil {
+
+		}
+	}(pool, resource)
+
+	//b, _ := json.Marshal(&models.User{Email: "a@a.com", Username: "a", Password: "pass"})
+
+	tests := []struct {
+		description string
+
+		// Test input
+		route string
+
+		// Expected output
+		expectedError bool
+		expectedCode  int
+		expectedBody  bool
+		evaluator     string
+		query         string
+		queryValue    string
+		body          []byte
+	}{
+		{
+			description:   "unauthorized",
+			route:         "/iriguchi/items",
+			expectedError: false,
+			expectedCode:  401,
+			expectedBody:  true,
+			evaluator:     "",
+			query:         "",
+			queryValue:    "",
+			body:          nil,
+		},
+		//{
+		//	description:   "logged in - create products",
+		//	route:         "/iriguchi/items",
+		//	expectedError: false,
+		//	expectedCode:  201,
+		//	expectedBody:  true,
+		//	evaluator:     "",
+		//	query:         "",
+		//	queryValue:    "",
+		//	body:          b,
+		//},
+	}
+
+	conn := database.ConnectToDB()
+	conn.AdminCollection.InsertOne(context.TODO(), &models.User{Email: "a@a.com", Username: "a", Password: "pass"})
+
+	// Setup the app as it is done in the main function
+	app := router.Setup()
+
+	// Iterate through test single test cases
+	for _, test := range tests {
+		// Create a new http request with the route
+		// from the test case
+		req, _ := http.NewRequest(
+			"POST",
+			test.route,
+			nil,
+		)
+
+		q := req.URL.Query()
+		q.Add(test.query, test.queryValue)
+		req.URL.RawQuery = q.Encode()
+
+		if strings.Contains(test.description, "logged in") {
+			data := url.Values{"email": {}}
+			data.Set("body", "{email: a@a.com, password: pass}")
+			re, _ := http.NewRequest(
+				"POST",
+				"/iriguchi/auth/login",
+				bytes.NewBufferString(data.Encode()),
+			)
+
+			myR, _ := app.Test(re, -1)
+
+			c := myR.Cookies()
+
+			fmt.Println(c)
+
+			req.AddCookie(c[0])
 		}
 
 		// Perform the request plain with the app.
